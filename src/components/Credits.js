fc6b72b95179e32a73c449ecdfc2677191ad4865
fatimajavid <fatimajavid101@gmail.com>
2022-11-22 13:49:08 -0500

    Changes from Jay's repo

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,19 +1,83 @@
 /*==================================================
 src/components/Credits.js
-
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
-const Credits = (props) => {
-  return (
-    <div>
-      <h1>Credits</h1>
-      <br/>
-      <Link to="/">Return to Home</Link>
-    </div>
-  )
+
+class CreditsComponent extends Component {
+  constructor () {
+    super()
+    this.state = {
+      credit: {
+          id: 'NEW',
+          amount: 0,
+          date: 'FILL ME',
+          description: 'DESCRIPTION',
+      },
+    }
+  }
+
+  creditsView = () => {
+    const { credits } = this.props;
+    return credits.map((credit) => {    
+      let date = credit.date.slice(0,10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    })
+  }
+
+  handleChange = (e) => {
+    const updatedCredit = {...this.state.credit}
+    const inputField = e.target.name
+    const inputValue = e.target.value
+    updatedCredit[inputField] = inputValue
+    let rightNow = new Date()
+    updatedCredit['date'] = rightNow.toISOString()
+    this.setState({credit: updatedCredit})
+  }
+  
+  handleSubmit  = (e) => {
+    e.preventDefault();
+
+    this.props.addCredit(this.state.credit);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Credits </h1>
+        {this.creditsView()}
+        <h1/>
+        <AccountBalance accountBalance={this.props.accountBalance}/>
+        <h1/>
+
+        <Link to="/userProfile">User Profile</Link>
+        <h1></h1>
+        <Link to="/LogIn">Log In</Link>
+        <h1></h1>
+        <Link to="/Debits">Debits</Link>
+        <h1></h1>
+        <Link to="/">Return to Home</Link>
+        <h1></h1>
+
+        <h1></h1>
+        <form onSubmit={this.handleSubmit}>
+            <div>
+            <label htmlFor="description"> Description: </label>
+                <input type="text" name="description" onChange={this.handleChange}/>
+            </div>
+            <div>
+            <label htmlFor="amount"> Amount: </label>
+                <input type="text" name="amount" onChange={this.handleChange}/>
+            </div>
+            <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Credits;
+export default CreditsComponent;
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,36 +1,82 @@
 /*==================================================
 src/components/Debits.js
-
 The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
-const Debits = (props) => {
-  // Create the list of Debit items
-  let debitsView = () => {
-    const { debits } = props;
-    return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+class DebitsComponent extends Component {
+  constructor () {
+    super()
+    this.state = {
+      debit: {
+          id: 'NEW',
+          amount: 0,
+          date: 'FILL ME',
+          description: 'DESCRIPTION',
+      },
+    }
+  }
+
+  debitsView = () => {
+    const { debits } = this.props;
+    return debits.map((debit) => {    
       let date = debit.date.slice(0,10);
       return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-    }) 
+    })
   }
-  // Render the list of Debit items and a form to input new Debit item
-  return (
-    <div>
-      <h1>Debits</h1>
 
-      {debitsView()}
+  handleChange = (e) => {
+    const updatedDebit = {...this.state.debit}
+    const inputField = e.target.name
+    const inputValue = e.target.value
+    updatedDebit[inputField] = inputValue
+    let rightNow = new Date()
+    updatedDebit['date'] = rightNow.toISOString()
+    this.setState({debit: updatedDebit})
+  }
+    
+  handleSubmit  = (e) => {
+    e.preventDefault();
 
-      <form onSubmit={props.addDebit}>
-        <input type="text" name="description" />
-        <input type="number" name="amount" />
-        <button type="submit">Add Debit</button>
-      </form>
-      <br/>
-      <Link to="/">Return to Home</Link>
-    </div>
-  )
+    this.props.addDebit(this.state.debit);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Debits </h1>
+        {this.debitsView()}
+        <h1/>
+        <AccountBalance accountBalance={this.props.accountBalance}/>
+        <h1/>
+
+        <Link to="/userProfile">User Profile</Link>
+        <h1></h1>
+        <Link to="/LogIn">Log In</Link>
+        <h1></h1>
+        <Link to="/Credits">Credits</Link>
+        <h1></h1>
+        <Link to="/">Return to Home</Link>
+        <h1></h1>
+
+        <h1></h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+          <label htmlFor="description"> Description: </label>
+              <input type="text" name="description" onChange={this.handleChange}/>
+          </div>
+          <div>
+          <label htmlFor="amount"> Amount: </label>
+              <input type="text" name="amount" onChange={this.handleChange}/>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Debits;
+export default DebitsComponent;
